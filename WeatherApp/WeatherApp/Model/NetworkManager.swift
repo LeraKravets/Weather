df317af621045fc1427f4480b225de0026f04444
Valeryia Kravets <kravetsvaleryia@gmail.com>
2019-09-23 22:38:16 +0300

    OpenWeather API used

--- WeatherApp/WeatherApp/Model/NetworkManager.swift
+++ WeatherApp/WeatherApp/Model/NetworkManager.swift
@@ -11,30 +11,21 @@ import Foundation
 class NetworkManager {
 
     static let shared = NetworkManager()
-//    private let ApiKey = "1dd60096c7aeeef497dea519ecf26df5"
 
     private init() {}
 
     func loadForecast(city: String, completionHandler: @escaping ([String: Any]?) -> Void) {
-        let resourceString =  "https://community-open-weather-map.p.rapidapi.com/weather?q=\(city)"
+//        let personalAPI = "5fd0c255bfc224e83c8160bb7241d760"
+        //let resourceString =  "https://community-open-weather-map.p.rapidapi.com/forecast?q=\(city)"
+        let resourceString =  "https://api.openweathermap.org/data/2.5/weather?q=\(city)&APPID=5fd0c255bfc224e83c8160bb7241d760"
 
-        let headers = [
-            "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-            "x-rapidapi-key": "469d7e00ffmsh86117e6c69776edp1fd2cajsnfb722761cf8e"
-        ]
         guard let resourceURL = URL(string: resourceString) else { fatalError() }
-        var request = URLRequest(url: resourceURL, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 10.0)
-        request.httpMethod = "GET"
-        request.allHTTPHeaderFields = headers
 
-        let dataTask = URLSession.shared.dataTask(with: request) { (data, _, error) in
+        let dataTask = URLSession.shared.dataTask(with: resourceURL) { (data, _, error) in
             guard let jsonData = data, error == nil else { return }
-//            guard let json = try JSONSerialization.jsonObject(with: jsonData, options: .mutableContainers)
-//                as? [String: Any] else { return }
             do {
                 guard let json = try JSONSerialization.jsonObject(with: jsonData, options: .mutableContainers)
                     as? [String: Any] else { return }
-//                let weatherResponse = try JSONDecoder().decode(WeatherItems.self, from: jsonData)
                 print(json)
                 completionHandler(json)
             } catch {
